ef6bb97bf15d03bc4372b7b1620c9853276867d3
Abba1100011 <francesco.abbate@stud.unifi.it>
2022-05-17 03:56:47 +0200

    Completato MinColumnTest

--- Test/MinColumnTest.cpp
+++ Test/MinColumnTest.cpp
@@ -1,36 +1,57 @@
 #include "gtest/gtest.h"
 #include "../MinColumn.h"
 
-TEST(MinColumnTest, unsignedTestOutput) {
+TEST(MinColumnTest, unsignedTestImpput) {
     MinColumn a;
     Cell b(5);
     Cell c(10);
     Cell d(4);
     b.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5);
     c.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5);
     d.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 4);
+    c.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 4);
+    b.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 4);
 
 }
 
-TEST(MinColumnTest, negativeTestOutput) {
+TEST(MinColumnTest, negativeTestImput) {
     MinColumn a;
     Cell b(-3);
     Cell c(-4);
     Cell d(-7);
     b.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -3);
     c.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -4);
     d.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -7);
+    c.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -7);
+    b.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -7);
 
 }
 
-TEST(MinColumnTest, floatTestOutput) {
+TEST(MinColumnTest, floatTestImtput) {
     MinColumn a;
     Cell b(5.0);
     Cell c(5.31);
     Cell d(5.3);
     b.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5.0);
     c.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5.0);
     d.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5.0);
+    c.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5.0);
+    b.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5.3);
 
 }
 
@@ -40,7 +61,14 @@ TEST(MinColumnTest, mixedTestOutput) {
     Cell c(-10);
     Cell d(4.4);
     b.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 5);
     c.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -10);
     d.Subscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), -10);
+    c.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 4.4);
+    b.Unsubscribe(&a);
+    EXPECT_FLOAT_EQ(a.getResult(), 4.4);
 
 }
