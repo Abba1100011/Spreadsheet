a6f0353dfa24880d29d479b918f88180a8b688a1
Abba1100011 <francesco.abbate@stud.unifi.it>
2022-05-20 17:31:57 +0200

    Modificata assertion su MeanColumnTest (ASSERT_NEAR) per confronto tra numeri float periodici e precisione di macchina dovuta a rapporto tra float (scelta come soglia per l'uguaglianza unità < della minima unita posizionale richiesta dal confronto).

--- Test/MeanColumnTest.cpp
+++ Test/MeanColumnTest.cpp
@@ -1,21 +1,23 @@
 #include "gtest/gtest.h"
 #include "../MeanColumn.h"
 
+#define PRECISION 0.0009
+
 TEST(MeanColumnTest, unsignedTestImput) {
     MeanColumn a;
     Cell b(5);
     Cell c(10);
     Cell d(4);
     b.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5);
+    EXPECT_NEAR(a.getResult(), 5.000, PRECISION);
     c.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 7.5);
+    EXPECT_NEAR(a.getResult(), 7.500, PRECISION);
     d.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 6.333335);
+    EXPECT_NEAR(a.getResult(), 6.333, PRECISION);
     c.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 4.5);
+    EXPECT_NEAR(a.getResult(), 4.500, PRECISION);
     b.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 4);
+    EXPECT_NEAR(a.getResult(), 4.000, PRECISION);
 
 }
 
@@ -25,15 +27,15 @@ TEST(MeanColumnTest, negativeTestImput) {
     Cell c(-4);
     Cell d(-7);
     b.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -3);
+    EXPECT_NEAR(a.getResult(), -3.000, PRECISION);
     c.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -3.5);
+    EXPECT_NEAR(a.getResult(), -3.500, PRECISION);
     d.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -4.666666);
+    EXPECT_NEAR(a.getResult(), -4.666, PRECISION);
     c.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -5);
+    EXPECT_NEAR(a.getResult(), -5.000, PRECISION);
     b.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -7);
+    EXPECT_NEAR(a.getResult(), -7.000, PRECISION);
 
 }
 
@@ -43,15 +45,15 @@ TEST(MeanColumnTest, floatTestImput) {
     Cell c(5.31);
     Cell d(5.3);
     b.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5);
+    EXPECT_NEAR(a.getResult(), 5.000, PRECISION);
     c.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5.155);
+    EXPECT_NEAR(a.getResult(), 5.155, PRECISION);
     d.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5.2033333);
+    EXPECT_NEAR(a.getResult(), 5.203, PRECISION);
     c.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5.15);
+    EXPECT_NEAR(a.getResult(), 5.150, PRECISION);
     b.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5.3);
+    EXPECT_NEAR(a.getResult(), 5.300, PRECISION);
 
 }
 
@@ -61,14 +63,14 @@ TEST(MeanColumnTest, mixedTestImput) {
     Cell c(-10);
     Cell d(4.4);
     b.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 5);
+    EXPECT_NEAR(a.getResult(), 5.000, PRECISION);
     c.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -2.5);
+    EXPECT_NEAR(a.getResult(), -2.500, PRECISION);
     d.Subscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), -0.2);
+    EXPECT_NEAR(a.getResult(), -0.200, PRECISION);
     c.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 4.7);
+    EXPECT_NEAR(a.getResult(), 4.700, PRECISION);
     b.Unsubscribe(&a);
-    EXPECT_FLOAT_EQ(a.getResult(), 4.4);
+    EXPECT_NEAR(a.getResult(), 4.400, PRECISION);
 
 }
